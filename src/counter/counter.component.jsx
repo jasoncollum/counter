b8df1bbb78bb21c8396e2cf58437bd2195e2eff3
Jason Collum <jasoncollum@gmail.com>
2020-01-02 15:56:12 -0600

    add Toggler component and useToggle custom hook

--- src/counter/counter.component.jsx
+++ src/counter/counter.component.jsx
@@ -5,7 +5,7 @@ const Counter = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
+        <div className='counter-container'>
             <h1>The Count is: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Plus 1</button>
             <button onClick={() => setCount(count - 1)}>Minus 1</button>
